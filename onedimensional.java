b7e41e8e4245d644160f6c611e5150fce2cb3cb9
LakshmiN17 <161114969+LakshmiN17@users.noreply.github.com>
2024-03-01 23:36:18 +0530

    Add files via upload

---
+++ onedimensional.java
@@ -0,0 +1,27 @@
+class onedimensional
+{
+	public static void main(String args[])
+	{  
+	   int a[]={10,20,30,40,50};    
+	   for(int i=0;i<a.length;i++) 
+       {
+        for (var i = 0; i < a.length; i++) 
+        {
+            for (var j = 0; j < a2.length; j++) 
+            {
+                if (i != j) 
+                {
+                    if (a.length[i] == a2.length[j]) 
+                    {
+                        return true; 
+                    }
+                }
+            }
+        }
+        return false; 
+       } 
+	   {
+		System.out.println("a["+i+"]:"+a[i]);    
+	   }	
+	}
+}
